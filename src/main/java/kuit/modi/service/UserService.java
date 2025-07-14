f783a5769d37866e687cd88616c724846499cb3a
simchaehyeon <tlacogussla@konkuk.ac.kr>
2025-07-14 22:12:56 +0900

    [FEAT] 기록 생성 기능(테스트 전)

--- src/main/java/kuit/modi/service/UserService.java
+++ src/main/java/kuit/modi/service/UserService.java
@@ -2,14 +2,14 @@ package kuit.modi.service;
 
 import kuit.modi.dto.UserRequest;
 import kuit.modi.dto.UserResponse;
-import kuit.modi.repository.UserRepository;
+import kuit.modi.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
-    private final UserRepository userRepository;
+    private final MemberRepository memberRepository;
 
 
     public UserResponse createUser(UserRequest requestDto) {
