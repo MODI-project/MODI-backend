6c91d58e2c118f4feac5f217a76fd3797bb4c36b
Jiye Lim <limgeeee02@naver.com>
2025-07-18 16:18:11 +0900

    [ADD] : AI 프롬프팅 관련 클래스 생성

---
+++ src/main/java/kuit/modi/dto/ai/AutoGenerateRequest.java
@@ -0,0 +1,10 @@
+package kuit.modi.dto.ai;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class AutoGenerateRequest {
+    private List<String> tags;
+}
---
+++ src/main/java/kuit/modi/dto/ai/LanguageStyleRequest.java
@@ -0,0 +1,8 @@
+package kuit.modi.dto.ai;
+
+import lombok.Getter;
+
+@Getter
+public class LanguageStyleRequest {
+    private String content;
+}
