496b067c6501aca6308c4ee5d4e048944d344350
Jiye Lim <limgeeee02@naver.com>
2025-07-22 00:57:45 +0900

    [ADD] : AI 프롬프팅 1차 구현

--- src/main/java/kuit/modi/dto/ai/AutoGenerateRequest.java
+++ src/main/java/kuit/modi/dto/ai/AutoGenerateRequest.java
@@ -1,10 +1,14 @@
 package kuit.modi.dto.ai;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AutoGenerateRequest {
     private List<String> tags;
 }
