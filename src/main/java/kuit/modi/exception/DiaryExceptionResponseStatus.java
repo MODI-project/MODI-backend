07ad66a2cbe257c761e00983670b6cee0343888e
simchaehyeon <tlacogussla@konkuk.ac.kr>
2025-08-06 11:53:35 +0900

    [REFACTOR] diaryService 에러응답 리팩토링

--- src/main/java/kuit/modi/exception/DiaryExceptionResponseStatus.java
+++ src/main/java/kuit/modi/exception/DiaryExceptionResponseStatus.java
@@ -7,9 +7,13 @@ import org.springframework.http.HttpStatus;
 @Getter
 @RequiredArgsConstructor
 public enum DiaryExceptionResponseStatus implements BaseExceptionResponseStatus {
-    DIARY_NOT_FOUND(10001, HttpStatus.NOT_FOUND, "해당 기록을 찾을 수 없습니다."),
+
+    DIARY_NOT_FOUND(10001, HttpStatus.NOT_FOUND, "일기를 찾을 수 없습니다."),
     INVALID_DATE(10002, HttpStatus.BAD_REQUEST, "유효하지 않은 날짜입니다."),
-    INVALID_YEAR_MONTH(10003, HttpStatus.BAD_REQUEST, "잘못된 연/월 형식입니다.");
+    INVALID_TONE(10003, HttpStatus.BAD_REQUEST, "톤 정보가 유효하지 않습니다."),
+    INVALID_EMOTION(10004, HttpStatus.BAD_REQUEST, "감정 정보가 유효하지 않습니다."),
+    INVALID_FRAME(10005, HttpStatus.BAD_REQUEST, "프레임 정보가 유효하지 않습니다."),
+    INVALID_LOCATION(10006, HttpStatus.BAD_REQUEST, "위치 정보가 유효하지 않습니다.");
 
     private final int code;
     private final HttpStatus httpStatus;
