8a4bfc5d1f43abc9527f2ed55da2424152b355ff
Lim Jiye <limgeeee02@naver.com>
2025-08-20 02:20:09 +0900

    [FIX] 많이 쓰이는 태그 조회 수정

--- src/main/java/kuit/modi/repository/TagRepository.java
+++ src/main/java/kuit/modi/repository/TagRepository.java
@@ -4,6 +4,7 @@ import kuit.modi.domain.Tag;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -19,5 +20,17 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
     ORDER BY COUNT(t.name) DESC
     """)
     List<String> findTopTagNames(Pageable pageable);
+
+    @Query("""
+        SELECT t.name 
+        FROM DiaryTag dt
+        JOIN dt.tag t
+        JOIN dt.diary d
+        WHERE d.member.id = :memberId
+        GROUP BY t.id, t.name
+        ORDER BY COUNT(dt) DESC
+    """)
+    List<String> findTopTagNamesByMemberId(@Param("memberId") Long memberId, Pageable pageable);
+
     Optional<Tag> findByNameIgnoreCase(String name);
 }
